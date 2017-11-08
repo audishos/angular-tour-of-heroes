8879d9928e3c3a039702c3ed2de30cbe4d7477da
audishos <audisho.sada@gmail.com>
2017-11-08 13:10:00 -0500

    Implements routing and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent }
 ]
 
